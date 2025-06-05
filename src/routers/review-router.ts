eff6506e424af8e52089d943c2d4decccb5b9519
L1SARD7 <arthem.klimchuk7@gmail.com>
2025-06-05 05:17:59 +0300

    added the ability to edit reviews from game page

--- src/routers/review-router.ts
+++ src/routers/review-router.ts
@@ -31,22 +31,23 @@ ReviewRouter.post('/:id',
     const validation = validationResult(req)
     if (validation.isEmpty()) {
         // @ts-ignore
-        const isAlreadyCreated = await ReviewRepository.FindReviewByUserId(Number(req.session.user.id))
-        if (isAlreadyCreated) {
+        const isAlreadyCreated: [] = await reviewService.GetReviews(req.params.id, +req.session.user.id)
+        if (isAlreadyCreated.length !== 0) {
             res.send('В вас вже є залишений відгук цій грі.')
-        }
-        // @ts-ignore
-        if (req.session.user) {
+        } else {
             // @ts-ignore
-            const CreatedReview = await reviewService.CreateNewReview(req.body.rating, req.body.text, +req.params.id, req.session.user.id, req.session.user.username)
-            if (CreatedReview) {
-                res.status(HTTP_CODES.Created_201).redirect(`/games/${req.params.id}`)
-            } else {
-                res.status(HTTP_CODES.BAD_REQUEST_400).redirect(`/`)
+            if (req.session.user) {
+                // @ts-ignore
+                const CreatedReview = await reviewService.CreateNewReview(req.body.rating, req.body.text, +req.params.id, req.session.user.id, req.session.user.username)
+                if (CreatedReview) {
+                    res.status(HTTP_CODES.Created_201).redirect(`/games/${req.params.id}`)
+                } else {
+                    res.status(HTTP_CODES.BAD_REQUEST_400).redirect(`/`)
+                }
             }
-        }
-        else {
-            res.send('Для того, щоб залишити відгук, необхідно бути авторизованим.')
+            else {
+                res.send('Для того, щоб залишити відгук, необхідно бути авторизованим.')
+            }
         }
     }
     else {
@@ -63,7 +64,7 @@ ReviewRouter.delete('/:id',
     }
     let isDeleted = await reviewService.DeleteReview(+req.params.id)
     if (isDeleted) {
-        res.status(HTTP_CODES.Deleted_204)
+        res.status(HTTP_CODES.Deleted_204).redirect(req.body.returnTo)
     }
     else {
         res.status(HTTP_CODES.BAD_REQUEST_400)
--- src/validator/ReviewInputDataValidator.ts
+++ src/validator/ReviewInputDataValidator.ts
@@ -1,4 +1,4 @@
 import { body } from "express-validator";
 
-export const bodyRatingReviewValidatorMiddleware = body('rating').trim().isNumeric().withMessage('Rating should be a number from 0 to 10.')
-export const bodyTextReviewValidatorMiddleware = body('text').trim().isLength({min: 3, max: 2000}).withMessage('Text should be from 3 to 64 letters.')
+export const bodyRatingReviewValidatorMiddleware = body('rating').trim().isInt({min: 0, max: 10}).withMessage('Rating should be a number from 0 to 10.')
+export const bodyTextReviewValidatorMiddleware = body('text').trim().isLength({max: 2000}).withMessage('Text should be from 3 to 64 letters.')
